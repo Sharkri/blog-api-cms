e505b644781f9a9e623cb7079f64faaafc99a7e8
Sharkri <sharkdude955@gmail.com>
2024-02-25 14:38:06 -0600

    feat: get ready for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://sharkri.github.io/blog-api-cms",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
